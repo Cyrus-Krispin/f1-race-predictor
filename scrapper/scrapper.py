ae6f7b8ea15766c0a54d551688d31088eaafebd3
Cyrus Krispin <cyruskrispin@gmail.com>
2024-06-01 16:21:15 +0800

    get name of drivers

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -2,5 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-r = requests.get("https://www.formula1.com/en/drivers")
-print(r.text)
+r = requests.get("https://www.formula1.com/en/drivers").text
+soup = BeautifulSoup(r, "lxml")
+
+first = soup.find_all("p", class_="f1-heading tracking-normal text-fs-12px leading-tight uppercase font-normal non-italic f1-heading__body font-formulaOne")
+last = soup.find_all("p", class_="f1-heading tracking-normal text-fs-18px leading-tight uppercase font-bold non-italic f1-heading__body font-formulaOne")
+
+f = BeautifulSoup(str(first), "lxml")
+l = BeautifulSoup(str(last), "lxml")
+
+print(f.get_text())
+print(l.get_text())

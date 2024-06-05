f559208c53c97f5ed136a7b6cb79ce7ee1df51fe
Cyrus Krispin <cyruskrispin@gmail.com>
2024-06-05 20:58:16 +0800

    trying out writing to csv file

--- scrapper/completed_races.py
+++ scrapper/completed_races.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 r = requests.get("https://www.formula1.com/en/results.html/2024/races.html").text
 soup = BeautifulSoup(r, "lxml")
@@ -12,3 +13,9 @@ races_list = BeautifulSoup(str(races), "lxml")
 
 for race in races_list.stripped_strings:
     print(race)
+
+# Writing
+
+with open("file.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(races_list.stripped_strings)

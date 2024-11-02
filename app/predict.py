a1b14ba1e4eb71365a9387c96b624d22a2973859
Cyrus Krispin <cyruskrispin@gmail.com>
2024-11-03 07:18:52 +0800

    only 16 percent

---
+++ app/predict.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+
+
+data = pd.read_csv("data.csv")
+
+data = data.dropna(subset=['race'])
+
+X = data[["race_name", "driver_number", "practice_1", "practice_2", "practice_3", "sprint_quali", "sprint_race", "qualifying"]]
+y = data['race']
+
+X = pd.get_dummies(X, columns=['driver_number', 'race_name'])
+
+imputer = SimpleImputer(strategy='median')
+X[['practice_1', 'practice_2', 'practice_3', 'sprint_quali', 'sprint_race', 'qualifying']] = imputer.fit_transform(X[['practice_1', 'practice_2', 'practice_3', 'sprint_quali', 'sprint_race', 'qualifying']])
+
+X['practice_avg'] = X[['practice_1', 'practice_2', 'practice_3']].mean(axis=1)
+X['practice_best'] = X[['practice_1', 'practice_2', 'practice_3']].min(axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+#i want csv file with the current data the model is using X and y
+X.to_csv("X.csv", index=False)
+
+# Example: Tuning hyperparameters in MLPRegressor
+model = RandomForestClassifier(n_estimators=200, max_features='sqrt', max_depth=15, min_samples_split=10, min_samples_leaf=5, class_weight='balanced',random_state=42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# mse = mean_squared_error(y_test, y_pred)
+# r2 = r2_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+
+# print(f"Mean Squared Error: {mse}")
+# print(f"R2 Score: {r2}")
+print(f"Accuracy: {accuracy}")
